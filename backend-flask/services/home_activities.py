7bdaa6f6a4ddf16a79bc85b49e4df15346a66f2b
Abheeshek Jain <abheedjain@gmail.com>
2023-03-04 07:25:34 +0000

    FIxed results_length -added indent to return stmt

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -48,6 +48,5 @@ class HomeActivities:
         'replies': []
       }
       ]
-    
-    span.set_attribute("app.result", len(results))
-    return results
+      span.set_attribute("app.result_length", len(results))
+      return results
