730dc68c6197139f3ff268660d35e47c70ef41ee
Abheeshek Jain <abheedjain@gmail.com>
2023-03-04 15:49:40 +0000

    Instrument Rollbar pass1

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,14 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+
+# Rollbar -----
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
+
 # Cloudwatch ----- 
 import watchtower
 import logging
@@ -38,6 +46,7 @@ from time import strftime
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
 
 # X-Ray -----
 # Remvoed for cost saving
@@ -95,6 +104,31 @@ cors = CORS(
 #    timestamp = strftime('[%Y-%b-%d %H:%M]')
 #    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #    return response
+
+# Rollbar -----
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+
+# Rolebar -----
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 
 @app.route("/api/message_groups", methods=['GET'])
