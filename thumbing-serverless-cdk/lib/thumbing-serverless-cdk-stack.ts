00ecb168c9c3ea1133538b4d8e192f9ccbdca5ac
Abheeshek Jain <abheedjain@gmail.com>
2023-04-08 18:05:19 +0000

    Pass 1 for AWS CDK - Lambda to add WP

---
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -0,0 +1,50 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import { Construct } from 'constructs';
+import * as dotenv from 'dotenv';
+// Load env variables
+//const dotenv = require('dotenv')
+dotenv.config();
+
+export class ThumbingServerlessCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
+    
+    const bucket = this.createBucket(bucketName);
+    const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput)
+  }
+
+  // goodo hav enew fun to make it easy to understand
+  createBucket(bucketName: string){
+    // const = calue does not change
+    //const = let stmt
+    const bucket = new s3.Bucket(this,'ThumbingBucket',{
+      bucketName: bucketName,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    return bucket;
+  }
+
+  // FUnction for Lambda
+//   createLambda(): cdk.lambda_layer_awscli.
+  createLambda(functionPath: string, bucketName: string, folderInput: string, folderOutput: string): lambda.IFunction{
+    const lambdaFunction = new lambda.Function(this, 'ThumbiLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(functionPath),
+      environment: {
+        DEST_BUCKET_NAME: bucketName,
+        FOLDER_INPUT: folderInput,
+        FOLDER_OUTPUT: folderOutput,
+        PROCESS_WIDTH: '512',
+        PROCESS_HEIGHT: '512'
+      }
+    });
+    return lambdaFunction;
+  }
+}
